dbfb23d78f182d99279a681e5006c2079593e766
thiago-bsws <thiago@bsws.net.br>
2014-08-31 17:39:34 -0300

    OpenMvc PHP Framework

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
+/**
+ * This file doesn't do anything, but loads the OpenMvc Framework Rote
+ */
 require_once("core/min.php");
 mapear_paginas($_SERVER['REQUEST_URI']);
 
