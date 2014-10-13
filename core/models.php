6b4f05965776e7070f1e474b7a35a09d4ccda8b5
thiago-bsws <thiago@bsws.net.br>
2014-10-13 16:47:04 -0300

    OpenMvc PHP Framework

--- core/models.php
+++ core/models.php
@@ -174,10 +174,9 @@ class Model extends Loader {
         $indexHeader = strpos($obj->$fieldFile, ';');
         $header = substr($obj->$fieldFile, 0, $indexHeader);
         $mimeType = str_replace('data:', '', $header);
-//        header('Content-Disposition: attachment; filename="' . $tableName . $fieldFile . $fileId . '.' . $this->mime_types_map(null, $mimeType) . '"');
-        header('Content-Disposition: attachment; filename="' . $tableName . $fieldFile . $fileId . '.' . $mimeType . '"');
+        header('Content-Disposition: attachment; filename="' . $tableName . $fieldFile . $fileId . '.' . $this->mime_types_map(null, $mimeType) . '"');
         header('Content-type: ' . $mimeType);
-        echo $fileContent = substr($obj->$fieldFile, $indexHeader + 1);
+        print $fileContent = substr($obj->$fieldFile, $indexHeader + 1);
     }
 
     public function get_filename_ext($filename) {
