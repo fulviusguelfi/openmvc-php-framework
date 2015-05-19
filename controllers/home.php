9e1bb227f5207c8719a77566fdcf4743c37d8562
thiago-bsws <thiago@bsws.net.br>
2015-05-19 00:17:23 -0300

    loadCrudGenerator

--- controllers/home.php
+++ controllers/home.php
@@ -24,6 +24,10 @@
 class Home extends Controller {
 
     public function index() {
+        $this->loadCrudGenerator();
+    }
+
+    public function loadCrudGenerator() {
         $this->load("models", "binModel");
         $tables = $this->binModel->getTables();
         $this->view("home/index", array("tables" => $tables));
