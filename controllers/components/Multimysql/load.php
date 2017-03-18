27ece8717fce95a8d877fd72dca88568444b5ee2
thiago <thiago@thiago-K84C>
2017-03-18 00:58:52 -0300

    add multimysql component

---
+++ controllers/components/Multimysql/load.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+  Este arquivo é parte do OpenMvc PHP Framework
+
+  OpenMvc PHP Framework é um software livre; você pode redistribuí-lo e/ou
+  modificá-lo dentro dos termos da Licença Pública Geral GNU como
+  publicada pela Fundação do Software Livre (FSF); na versão 2 da
+  Licença, ou (na sua opinião) qualquer versão.
+
+  Este programa é distribuído na esperança de que possa ser  útil,
+  mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO a qualquer
+  MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+  Licença Pública Geral GNU para maiores detalhes.
+
+  Você deve ter recebido uma cópia da Licença Pública Geral GNU
+  junto com este programa, se não, escreva para a Fundação do Software
+  Livre(FSF) Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+//        DEFINIR AS CONSTANTES ABAIXO P CONFIGURAR O COMPONENT
+//        $dbuser = MULTIMYSQL_DBUSER;
+//        $dbpassword = MULTIMYSQL_DBPASS;
+//        $dbname = MULTIMYSQL_DBNAME;
+//        $dbhost = MULTIMYSQL_DBHOST;
+include substr(__DIR__, 0, -33) . "/controllers/components/Multimysql/src/multimysql.php";
+
+
